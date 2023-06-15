da6e0610b2c5de5db95373efa4fed5663fd69ba2
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-14 21:41:11 -0300

    se crea componente header

--- components/header.js
+++ components/header.js
@@ -9,21 +9,35 @@ class Header extends HTMLElement {
 
         <header class="header-container">
             <figure class="container-icon">
-            <a class="logo-href" href="#"><img src="assets/icon/Header_Logo.png" class="logo" /></a>
+                <a class="logo-href" href="#"><img src="assets/icon/Header_Logo.png" class="logo" /></a>
             </figure>
             <div class="searchbox_container">
-            <table class="searchboxelements">
-                <tr>
-                <td>
-                    <input type="text" placeholder="Buscar" class="search">
-                </td>
-                <td class="searchicon">
-                    <a class="material-symbols-outlined">
-                    search
-                    </a>
-                </td>
-                </tr>
-            </table>
+                <table class="searchboxelements">
+                    <tr>
+                    <td>
+                        <input type="text" placeholder="Buscar" class="search">
+                    </td>
+                    <td class="searchicon">
+                        <a class="material-symbols-outlined">
+                        search
+                        </a>
+                    </td>
+                    </tr>
+                </table>
+            </div>
+            </div>
+                <div class="buttons">
+                <button class="login-button">
+                    Iniciar sesión
+                    <span class="material-symbols-outlined material-icon" >
+                        person
+                    </span>
+                </button>
+                <button class="cart-button">
+                    <span class="material-symbols-outlined material-icon">
+                        shopping_cart
+                    </span>
+                </button>
             </div>
         </header>
         `
