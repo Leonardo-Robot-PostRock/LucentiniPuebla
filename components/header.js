4f17767b2961189b58ddcf61fdd6f2b27b29252f
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-15 05:36:30 -0300

    se mejora custom element

--- components/header.js
+++ components/header.js
@@ -1,64 +1,63 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+<header class="header-container">
+    <section class="header-container__background">
+        <figure class="container-icon">
+            <a class="logo-href" href="#"><img src="assets/icon/Header_Logo.png" class="logo" /></a>
+        </figure>
+        <div class="search-box-container">
+            <form class="search-box-elements">
+                <label class="search-input">
+                    <input type="text" placeholder="Buscar" class="search">
+                </label>
+                <figure class="search-icon">
+                    <a class="material-symbols-outlined">
+                        search
+                    </a>
+                </figure>
+            </form>
+        </div>
+        <div class="buttons">
+            <button class="login-button">
+                Iniciar sesión
+                <span class="material-symbols-outlined material-icon" >
+                person
+                </span>
+            </button>
+            <button class="cart-button">
+                <span class="material-symbols-outlined material-icon">
+                    shopping_cart
+                </span>
+            </button>
+        </div>
+    </section>
+    <nav class="navigation">
+        <ul class="container-li">
+            <li class="list"><a href="/index.html" id="link home">Home</a></li>
+            <li class="list"><a href="/products.html" id="link products">Productos</a></li>
+            <li class="list"><a href="/contacto.html" id="link contacto">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
+`
 class Header extends HTMLElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
-        this.innerHTML = `
-        <link rel="stylesheet" href="/styles/header.css"/>
+        const shadowRoot = this.attachShadow({ mode: 'closed' });
 
-        <header class="header-container">
-            <section class="header-container__background">
-                <figure class="container-icon">
-                    <a class="logo-href" href="#"><img src="assets/icon/Header_Logo.png" class="logo" /></a>
-                </figure>
-                <div class="search-box-container">
-                    <form class="search-box-elements">
-                        <label class="search-input">
-                            <input type="text" placeholder="Buscar" class="search">
-                        </label>
-                        <figure class="search-icon">
-                            <a class="material-symbols-outlined">
-                                search
-                            </a>
-                        </figure>
-                    </form>
-                </div>
-                <div class="buttons">
-                    <button class="login-button">
-                        Iniciar sesión
-                        <span class="material-symbols-outlined material-icon" >
-                        person
-                        </span>
-                    </button>
-                    <button class="cart-button">
-                        <span class="material-symbols-outlined material-icon">
-                            shopping_cart
-                        </span>
-                    </button>
-                </div>
-            </section>
-            <nav class="navigation">
-                <ul class="container-li">
-                    <li class="list"><a href="/index.html" id="link home" aria-current="page">Home</a></li>
-                    <li class="list"><a href="/products.html" id="link products">Productos</a></li>
-                    <li class="list"><a href="/contacto.html" id="link contacto">Contacto</a></li>
-                </ul>
-            </nav>
-        </header>
-        `
-        function navigateTo(url) {
-            window.location.href = url;
-        }
+        const styleElement = document.createElement('style');
 
-        const links = this.querySelectorAll('#link').forEach(link => {
-            console(link.href);
-            link.addEventListener('click', (event) => {
-                event.preventDefault();
-                const href = link.getAttribute('href');
-                navigateTo(href);
-            });
-        });
+        styleElement.textContent = `@import '/styles/header.css';
+            @import url(https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200);
+            @import url(https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;800&display=swap);
+        `;
+
+        shadowRoot.appendChild(styleElement);
+        shadowRoot.appendChild(headerTemplate.content.cloneNode(true));
     }
 }
 
