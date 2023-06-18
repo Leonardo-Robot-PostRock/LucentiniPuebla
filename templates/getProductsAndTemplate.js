dbc9dbc1259cd6d6b5e453d7e3602a53daafdd8e
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-18 05:31:51 -0300

    eventos para boton go home

--- templates/getProductsAndTemplate.js
+++ templates/getProductsAndTemplate.js
@@ -16,7 +16,6 @@ const renderProducts = async (data) => {
     gridElement.className = 'animate__animated animate__fadeIn';
     gridElement.innerHTML = ''
 
-
     data.forEach((product) => {
         const productElement = document.createElement('div');
         productElement.classList.add('product');
@@ -31,7 +30,6 @@ const renderProducts = async (data) => {
         imgElement.className = 'product-image';
         productElement.appendChild(imgElement);
 
-
         const priceElement = document.createElement('p');
         priceElement.textContent = `Price: $${product.price}`;
         priceElement.className = 'product-price';
@@ -42,12 +40,32 @@ const renderProducts = async (data) => {
         shoppingCartElement.textContent = 'add_shopping_cart';
         productElement.appendChild(shoppingCartElement);
 
-
         const buyElement = document.createElement('div');
         buyElement.classList.add('buy-container');
         buyElement.appendChild(priceElement);
         buyElement.appendChild(shoppingCartElement);
         productElement.appendChild(buyElement);
+
+        const nortSpanElement = document.createElement('span');
+        nortSpanElement.className = 'material-symbols-outlined nort-span';
+        nortSpanElement.textContent = 'north';
+
+        const goHomeElement = document.createElement('a');
+        goHomeElement.className = 'go-home';
+        goHomeElement.appendChild(nortSpanElement);
+        gridElement.appendChild(goHomeElement);
+
+        goHomeElement.onclick = function () {
+            window.scrollTo({ top: 0, behavior: 'smooth' })
+        }
+
+        window.addEventListener('scroll', function () {
+            if (window.pageYOffset > 0) {
+                goHomeElement.style.display = 'flex';
+            } else {
+                goHomeElement.style.display = 'none';
+            }
+        });
 
         gridElement.appendChild(productElement);
     });
