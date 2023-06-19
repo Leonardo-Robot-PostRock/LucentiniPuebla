e2ca9bdf7fb38258d61ba2b2b0f99637caae5588
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-19 17:07:55 -0300

    add footer custom Element

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,4 @@
-const footerTemplate = document.createElement('footer');
+const footerTemplate = document.createElement('template');
 
 footerTemplate.innerHTML = `
 <footer class="site-footer">
@@ -33,23 +33,23 @@ footerTemplate.innerHTML = `
 </footer>
 `
 class Footer extends HTMLElement {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    connectedCallback() {
-        const shadowRoot = this.attachShadow({ mode: 'closed' });
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
 
-        const styleElement = document.createElement('style');
+    const styleElement = document.createElement('style');
 
-        styleElement.textContent = `@import '/styles/footer.css';
+    styleElement.textContent = `@import '/styles/footer.css';
             @import url(https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200);
             @import url(https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;800&display=swap);
         `;
 
-        shadowRoot.appendChild(styleElement);
-        shadowRoot.appendChild(footerTemplate.content.cloneNode(true));
-    }
+    shadowRoot.appendChild(styleElement);
+    shadowRoot.appendChild(footerTemplate.content.cloneNode(true));
+  }
 }
 
 customElements.define('footer-component', Footer);
