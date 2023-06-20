73b405e9c063c084d042d5f938cafdf7c56d9007
Leonardo Puebla <leonardo38361@gmail.com>
2023-06-20 01:54:03 -0300

    se arregla import

--- components/header.js
+++ components/header.js
@@ -20,7 +20,7 @@ headerTemplate.innerHTML = `
         </div>
         <div class="buttons">
             <button class="login-button">
-                Iniciar sesión
+                Iniciar sesi&oacute;n
                 <span class="material-symbols-outlined material-icon" >
                 person
                 </span>
@@ -34,9 +34,9 @@ headerTemplate.innerHTML = `
     </section>
     <nav class="navigation">
         <ul class="container-li">
-            <li class="list"><a href="/index.html" id="link home">Home</a></li>
-            <li class="list"><a href="/products.html" id="link products">Productos</a></li>
-            <li class="list"><a href="/contacto.html" id="link contacto">Contacto</a></li>
+            <li class="list"><a href="./index.html" id="link home">Home</a></li>
+            <li class="list"><a href="./products.html" id="link products">Productos</a></li>
+            <li class="list"><a href="./contacto.html" id="link contacto">Contacto</a></li>
         </ul>
     </nav>
 </header>
@@ -51,7 +51,7 @@ class Header extends HTMLElement {
 
         const styleElement = document.createElement('style');
 
-        styleElement.textContent = `@import '/styles/header.css';
+        styleElement.textContent = `@import 'styles/header.css';
             @import url(https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200);
             @import url(https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;800&display=swap);
         `;
